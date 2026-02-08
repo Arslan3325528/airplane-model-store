a20a55284e321755411c4d883ef9224c71bb11aa
Ruslan379 <3325528@ukr.net>
2026-02-08 04:22:35 +0200

    step6

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Section from './components/Section';
+function App() {
+  const [count, setCount] = useState(0)
 
-//? Paintings
-import PaintingList from './components/PaintingList';
-import paintings from './json/paintings.json';
-
-
-//? Planes
-import PlanesList from './components/PlanesList';
-import planes from './json/planes.json';
-
-
-export default function App() {
   return (
     <>
-      {/* //? Paintings */}
-      <Section title="Колекція картин">
-        <PaintingList items={paintings} />
-      </Section >
-
-      {/* //? Planes */}
-      <Section title="Колекція моделей літаків">
-        <PlanesList items={planes} />
-      </Section >
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
+
+export default App
